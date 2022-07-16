fa2c41f2e0e3897dd2cd1138a0cc4d1046981d9e
Andres T <ag.torresc1@uniandes.edu.co>
2022-07-16 09:55:11 -0500

    Codigo con filtros Alejandro

--- Script_PS3.R
+++ Script_PS3.R
@@ -16,10 +16,39 @@ p_load(tidyverse, # manipular/limpiar conjuntos de datos.
 
 #1 Obtener datos-------
 ## descomprimir archivo
-unzip(zipfile="dataPS3.zip",
-      exdir=".")
+
+test <- readRDS("test.Rds")
+train <- readRDS("train.Rds")
+
 glimpse(test)
 glimpse(train)
+
+table(test$title)
+
+## Filtrar por Chapinero y El Poblado -----
+test$localidad_filtrada_chapinero <- grepl("chapinero", test$title, ignore.case= T)
+test$localidad_filtrada_poblado <- grepl("el poblado|poblado", test$title, ignore.case= T)
+
+test$localidad <- ifelse(test$localidad_filtrada_chapinero == TRUE, "CHAPINERO",
+                         ifelse(test$localidad_filtrada_poblado == TRUE, "POBLADO","NO APLICA"))
+
+##
+train$localidad_filtrada_chapinero <- grepl("chapinero", train$title, ignore.case= T)
+train$localidad_filtrada_poblado <- grepl("el poblado|poblado", train$title, ignore.case= T)
+
+train$localidad <- ifelse(train$localidad_filtrada_chapinero == TRUE, "CHAPINERO",
+                          ifelse(train$localidad_filtrada_poblado == TRUE, "POBLADO","NO APLICA"))
+
+
+
+
+
+table(test$localidad)
+table(train$localidad)
+
+test_final <- subset(test, localidad == "CHAPINERO"|localidad== "POBLADO")
+train_final <- subset(train, localidad == "CHAPINERO"|localidad== "POBLADO")
+table(test_final$localidad)
 
 available_features() %>% head(20)
 available_tags("amenity") %>% head(20)
@@ -38,9 +67,3 @@ bus_station = osm_sf$osm_points %>% select(osm_id,amenity)
 bus_station
 ## Pintar las estaciones de autobus
 leaflet() %>% addTiles() %>% addCircleMarkers(data=bus_station , col="red")
-
-view(osm_sf)
-
-
-
-
